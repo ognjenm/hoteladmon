753c72d3118dadcd31d90eb34e5b6ed4b255198b
chinux <lee_geing@hotmail.com>
2015-01-28 18:47:05 -0600

    creando el formulario de cancelacion

--- protected/views/reservation/_cancel.php
+++ protected/views/reservation/_cancel.php
@@ -8,21 +8,54 @@
 ?>
 
 <div class="form">
-<?php echo CHtml::beginForm(); ?>
+    <?php echo CHtml::beginForm(); ?>
 
-    <div class="row">
-        <?php echo CHtml::label('Fecha','fecha',array()) ?>
-        <?php echo CHtml::TextField('username','',array()) ?>
-    </div>
+            <?php echo CHtml::label('Fecha','date',array()) ?>
+            <?php //echo CHtml::TextField('username','',array()) ?>
 
-    <div class="form-actions">
-        <?php  $this->widget('bootstrap.widgets.TbButton', array(
-            'buttonType'=>'submit',
-            'type'=>'primary',
-            'icon'=>'icon-ok',
-            'label'=>Yii::t('mx','Save')
-        )); ?>
-    </div>
+            <?php $this->widget('bootstrap.widgets.TbDatePicker',
+                array('name' => 'date',
+                    'htmlOptions' => array('placeholder'=>Yii::t('mx','Date')),
+                    'options'=>array(
+                        'format'=>'dd-M-yyyy',
+                        'autoclose' => true,
 
-<?php echo CHtml::endForm(); ?>
+                    )
+                )
+            );
+            ?>
+    <?php
+
+    /*$this->widget('application.extensions.CJuiDateTimePicker.CJuiDateTimePicker',array(
+        'model'=>0,
+        'attribute'=>'datex',
+        'mode'=>'datetime' ,
+        'options'=>array(
+            'showAnim'=>'slide',
+            'changeYear' => true,
+            'changeMonth' => true,
+            'dateFormat'=>Yii::app()->format->dateFormat,
+            'timeText'=> Yii::t('mx','Schedule'),
+            'hourText'=> Yii::t('mx','Hour'),
+            'minuteText'=>Yii::t('mx','Minute'),
+            'timeOnlyTitle'=>Yii::t('mx','Choose Time'),
+            'minDate'=>0,
+            'hour'=>15,
+            'minute'=>0,
+        )
+    ));*/
+
+
+    ?>
+
+        <div class="form-actions">
+            <?php  $this->widget('bootstrap.widgets.TbButton', array(
+                'buttonType'=>'submit',
+                'type'=>'primary',
+                'icon'=>'icon-ok',
+                'label'=>Yii::t('mx','Cancel')
+            )); ?>
+        </div>
+
+    <?php echo CHtml::endForm(); ?>
 </div><!-- form -->
